37b96577b9a5a2f12a33d150a50d8343d78ed504
bkuzma <hello@briankuzma.com>
2019-03-25 16:57:23 -0400

    refactor App.js to use Query component rather than HOC

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
+import { graphql, Query } from "react-apollo";
 import gql from "graphql-tag";
+
+export const query = gql`
+  query ErrorTemplate {
+    people {
+      id
+      name
+    }
+  }
+`;
 
 class App extends Component {
   render() {
-    const { data: { loading, people } } = this.props;
     return (
-      <main>
-        <header>
-          <h1>Apollo Client Error Template</h1>
-          <p>
-            This is a template that you can use to demonstrate an error in
-            Apollo Client. Edit the source code and watch your browser window
-            reload with the changes.
-          </p>
-          <p>
-            The code which renders this component lives in{" "}
-            <code>./src/App.js</code>.
-          </p>
-          <p>
-            The GraphQL schema is in <code>./src/graphql/schema</code>.
-            Currently the schema just serves a list of people with names and
-            ids.
-          </p>
-        </header>
-        {loading ? (
-          <p>Loading…</p>
-        ) : (
-          <ul>
-            {people.map(person => <li key={person.id}>{person.name}</li>)}
-          </ul>
+      <Query query={query}>
+        {({ loading, error, data: { people } }) => (
+          <main>
+            <header>
+              <h1>Apollo Client Error Template</h1>
+              <p>
+                This is a template that you can use to demonstrate an error in
+                Apollo Client. Edit the source code and watch your browser window
+                reload with the changes.
+              </p>
+              <p>
+                The code which renders this component lives in{" "}
+                <code>./src/App.js</code>.
+              </p>
+              <p>
+                The GraphQL schema is in <code>./src/graphql/schema</code>.
+                Currently the schema just serves a list of people with names and
+                ids.
+              </p>
+            </header>
+            {loading ? (
+              <p>Loading…</p>
+            ) : (
+              <ul>
+                {people.map(person => <li key={person.id}>{person.name}</li>)}
+              </ul>
+            )}
+          </main>
         )}
-      </main>
+      </Query>
     );
   }
 }
 
-export default graphql(
-  gql`
-    query ErrorTemplate {
-      people {
-        id
-        name
-      }
-    }
-  `
-)(App);
+export default App;
